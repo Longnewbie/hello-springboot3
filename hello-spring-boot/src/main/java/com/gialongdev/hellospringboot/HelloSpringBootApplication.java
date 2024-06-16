0b8127afc29ba580ce0b0f369f530b8c2dac1ba7
lygialong <lygialong201@gmail.com>
2024-06-16 16:32:41 +0700

    Test444

--- hello-spring-boot/src/main/java/com/gialongdev/hellospringboot/HelloSpringBootApplication.java
+++ hello-spring-boot/src/main/java/com/gialongdev/hellospringboot/HelloSpringBootApplication.java
@@ -9,6 +9,7 @@ public class HelloSpringBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringBootApplication.class, args);
 		System.out.println("hello spring boot333");
+		System.out.println("STV CHAMPION");
 	}
 
 }
